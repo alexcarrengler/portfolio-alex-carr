bf1f1b8d741f3abc5530fe0b7a334929065cee40
Miles Grossenbacher <mrg498@nyu.edu>
2021-02-22 18:01:17 -0500

    ✨ sliding nav

--- gatsby-portfolio/src/components/header.js
+++ gatsby-portfolio/src/components/header.js
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import {Link} from "gatsby";
 import scrollTo from "gatsby-plugin-smoothscroll";
 import { FaAngleUp } from "@react-icons/all-files/fa/FaAngleUp";
 
 import navImg from "../images/navbar-img.png";
 // import {Link} from "gatsby"
-
 import headerStyle from "../components/header.module.scss";
 
 export default function Header() {
@@ -15,12 +15,21 @@ export default function Header() {
 			console.log("toggled");
 			return !toggled;
 		});
-	};
+	}
 
 	return (
 		<div>
 			<div id="top" />
 			<header className={headerStyle.component}>
+				<div className={
+							toggled ? `${headerStyle.navSlider} ${headerStyle.navOpen}` : `${headerStyle.navSlider}`
+						}>
+					<ul className={headerStyle.navLinks}>
+						<li><Link onClick={toggleOverlay} className={headerStyle.navLink} to="/">Home</Link></li>
+						<li><Link onClick={toggleOverlay} className={headerStyle.navLink} to="#scroll-about">About</Link></li>
+						<li><Link onClick={toggleOverlay} className={headerStyle.navLink} to="#scroll-contact">Contact</Link></li>
+					</ul>	
+				</div>
 				<nav className={headerStyle.nav}>
 					<div className={headerStyle.imgContainer} role="link" onClick={() => scrollTo("#top")}>
 						<img src={navImg} alt="nav img" className={headerStyle.img} />
@@ -38,11 +47,12 @@ export default function Header() {
 							toggleOverlay();
 						}}
 					>
-						<div className={headerStyle.hamburgerLine}/>
 						<div className={headerStyle.hamburgerLine} />
-						<div className={headerStyle.hamburgerLine}/>
+						<div className={headerStyle.hamburgerLine} />
+						<div className={headerStyle.hamburgerLine} />
 					</div>
 				</nav>
+					
 			</header>
 		</div>
 	);
