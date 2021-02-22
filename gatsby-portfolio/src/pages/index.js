b22d345223c433654c2c505858fa865c4967e091
Miles Grossenbacher <mrg498@nyu.edu>
2021-02-22 13:19:42 -0500

    📱 intro page responsive

--- gatsby-portfolio/src/pages/index.js
+++ gatsby-portfolio/src/pages/index.js
@@ -8,6 +8,7 @@ import ContactForm from "../components/contactForm";
 
 import { FaAngleDown } from "@react-icons/all-files/fa/FaAngleDown";
 
+import "../main.scss";
 import indexStyle from "./index.module.scss";
 import backgroundImageStyle from "../components/imageComponents/backgroundImage.module.scss";
 
@@ -28,31 +29,48 @@ const IndexPage = () => {
 							allowFullScreen
 						/>
 					</div>
+					<div className={`${indexStyle.iframeDesktop} align-center`}>
+						<iframe
+							title="index-vid"
+							className={indexStyle.iframeFullSize}
+							width='840'
+							height='472'
+							src="https://www.youtube.com/embed/fIidcLH2Xeo"
+							frameBorder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowFullScreen
+						/>
+					</div>
 					<div
 						className={`${indexStyle.seeMoreWrapper}`}
 						role="link"
 						onClick={() => scrollTo("#scroll-about")}
 					>
 						<h3 className={`${indexStyle.info} align-center`}>See More</h3>
-						<FaAngleDown className="align-center" style={{ fontSize: "32px" }} />
+						<FaAngleDown className={`${indexStyle.infoIcon} align-center`} />
 					</div>
 				</FullBackground>
 
-				<FullBackground className={`${backgroundImageStyle.componentBlock} ${backgroundImageStyle.colorInverse}`}>
-					<div id="scroll-about">
-						<h2 className={`${indexStyle.sectionTitle}`}>Hello!</h2>
+				<FullBackground
+					className={`${backgroundImageStyle.componentBlock} ${backgroundImageStyle.colorInverse}`}
+				>
+					<div className="pageContainer">
+						<div id="scroll-about">
+							<h2 className={`${indexStyle.sectionTitle}`}>Hello!</h2>
+						</div>
+						<p className={indexStyle.aboutText}>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Placerat vestibulum lectus mauris ultrices eros. Ultricies
+							integer quis auctor elit sed vulputate mi sit. Sit amet massa vitae tortor. Pretium aenean
+							pharetra magna ac placerat vestibulum lectus mauris ultrices. Vitae suscipit tellus mauris a
+							diam maecenas sed enim. Faucibus ornare suspendisse sed nisi lacus. Leo duis ut diam quam
+							nulla porttitor massa id neque. Condimentum id venenatis a condimentum vitae sapien
+							pellentesque habitant. Tortor condimentum lacinia quis vel eros donec. Egestas fringilla
+							phasellus faucibus scelerisque eleifend donec pretium. Elementum nibh tellus molestie nunc
+							non blandit massa enim. Habitant morbi tristique senectus et netus. Tristique nulla aliquet
+							enim tortor at.
+						</p>
 					</div>
-					<p className={indexStyle.aboutText}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Placerat vestibulum lectus mauris ultrices eros. Ultricies
-						integer quis auctor elit sed vulputate mi sit. Sit amet massa vitae tortor. Pretium aenean
-						pharetra magna ac placerat vestibulum lectus mauris ultrices. Vitae suscipit tellus mauris a
-						diam maecenas sed enim. Faucibus ornare suspendisse sed nisi lacus. Leo duis ut diam quam nulla
-						porttitor massa id neque. Condimentum id venenatis a condimentum vitae sapien pellentesque
-						habitant. Tortor condimentum lacinia quis vel eros donec. Egestas fringilla phasellus faucibus
-						scelerisque eleifend donec pretium. Elementum nibh tellus molestie nunc non blandit massa enim.
-						Habitant morbi tristique senectus et netus. Tristique nulla aliquet enim tortor at.
-					</p>
 				</FullBackground>
 				{/* <FullBackground className={`${backgroundImageStyle.componentBlock} ${backgroundImageStyle.colorRotate2}`}>
 					<div id="scroll-projects">
@@ -60,12 +78,16 @@ const IndexPage = () => {
 					</div>
 					<div className={indexStyle.projectsWrapper}></div>
 				</FullBackground > */}
-				<FullBackground className={`${backgroundImageStyle.componentBlock} ${backgroundImageStyle.colorRotate}`}>
-					<div id="scroll-contact">
-						<h2 className={`${indexStyle.sectionTitle}`}>Contact</h2>
-					</div>
-					<div>
-						<ContactForm />
+				<FullBackground
+					className={`${backgroundImageStyle.componentFlex} ${backgroundImageStyle.colorRotate}`}
+				>
+					<div className="pageContainer">
+						<div id="scroll-contact">
+							<h2 className={`${indexStyle.sectionTitle} text-center`}>Message Me</h2>
+						</div>
+						<div>
+							<ContactForm />
+						</div>
 					</div>
 				</FullBackground>
 			</section>
