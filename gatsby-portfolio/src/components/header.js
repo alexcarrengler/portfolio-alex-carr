93511e1b2c52a3eb679da4417bc8c7764e5f11e6
Miles Grossenbacher <mrg498@nyu.edu>
2021-02-22 10:28:07 -0500

    ✨ hamburger icon and animation

--- gatsby-portfolio/src/components/header.js
+++ gatsby-portfolio/src/components/header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import scrollTo from "gatsby-plugin-smoothscroll";
 import { FaAngleUp } from "@react-icons/all-files/fa/FaAngleUp";
 
@@ -7,20 +7,43 @@ import navImg from "../images/navbar-img.png";
 
 import headerStyle from "../components/header.module.scss";
 
-const Header = () => (
-	<div>
-		<div id="top" />
-		<header className={headerStyle.component}>
-			<nav className={headerStyle.nav}>
-				<div className={headerStyle.imgContainer} role="link" onClick={() => scrollTo("#top")}>
-					<img src={navImg} alt="nav img" className={headerStyle.img} />
-					<div className={headerStyle.middle}>
-						<div className={headerStyle.text}><FaAngleUp/></div>
-					</div>
-				</div>
-			</nav>
-		</header>
-	</div>
-);
+export default function Header() {
+	const [ toggled, setToggled ] = useState(false);
 
-export default Header;
+	const toggleOverlay = () => {
+		setToggled((toggled) => {
+			console.log("toggled");
+			return !toggled;
+		});
+	};
+
+	return (
+		<div>
+			<div id="top" />
+			<header className={headerStyle.component}>
+				<nav className={headerStyle.nav}>
+					<div className={headerStyle.imgContainer} role="link" onClick={() => scrollTo("#top")}>
+						<img src={navImg} alt="nav img" className={headerStyle.img} />
+						<div className={headerStyle.middle}>
+							<div className={headerStyle.text}>
+								<FaAngleUp />
+							</div>
+						</div>
+					</div>
+					<div
+						className={
+							toggled ? `${headerStyle.hamburger} ${headerStyle.isActive}` : `${headerStyle.hamburger}`
+						}
+						onClick={() => {
+							toggleOverlay();
+						}}
+					>
+						<div className={headerStyle.hamburgerLine}/>
+						<div className={headerStyle.hamburgerLine} />
+						<div className={headerStyle.hamburgerLine}/>
+					</div>
+				</nav>
+			</header>
+		</div>
+	);
+}
