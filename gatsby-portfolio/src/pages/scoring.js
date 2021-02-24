20d04cdb7493d88ce82887172d76eb121a774b60
Miles Grossenbacher <mrg498@nyu.edu>
2021-02-24 12:57:55 -0500

    ✨ project thumbnails link

--- gatsby-portfolio/src/pages/scoring.js
+++ gatsby-portfolio/src/pages/scoring.js
@@ -12,9 +12,11 @@ import TestImage from "../images/thumbnails/scoring/000_scoring-th.png";
 
 const projects = [
   {
+    link: ``,
     thumbnail: ``,
     image: ``,
-    title: `project1`,
+    thumbnailTitle: `project1`,
+    title: ``,
     credits: `Will Kahn, Alex Carr`,
     projectDescription: ``,
     personalDescription: ``
@@ -24,17 +26,45 @@ const projects = [
 
 const Scoring = () => (
   <div className={projectsStyles.page} style={{backgroundColor: `rgb(172, 196, 196)`}}>
-    <SEO title="Scoring" />
+    <SEO id="top" title="Scoring" />
     <section>
       <h1 className={`${pageStyles.title} ${projectsStyles.pageTitle}`}>Scoring</h1>
       <div className={`${projectsStyles.projectsContainer}`}>
           <div className={`${projectsStyles.projectWrapper}`}>
+            <Link to='/' className={`${projectsStyles.projectLink}`}>
             <img className={projectsStyles.thumbnail}  src={TestImage}/>
-            <p className={projectsStyles.projectTitle}>The Grove</p> 
+            <p  className={projectsStyles.projectTitle}>The Grove</p> 
+            </Link>
           </div>
           <div className={`${projectsStyles.projectWrapper}`}>
+            <Link to='/performance' className={`${projectsStyles.projectLink}`}>
             <img className={projectsStyles.thumbnail}  src={TestImage}/>
-            <p className={projectsStyles.projectTitle}>The Grove</p> 
+            <p  className={projectsStyles.projectTitle}>The Grove</p> 
+            </Link>
+          </div>
+          <div className={`${projectsStyles.projectWrapper}`}>
+            <Link to='/' className={`${projectsStyles.projectLink}`}>
+            <img className={projectsStyles.thumbnail}  src={TestImage}/>
+            <p  className={projectsStyles.projectTitle}>The Grove</p> 
+            </Link>
+          </div>
+          <div className={`${projectsStyles.projectWrapper}`}>
+            <Link to='/performance' className={`${projectsStyles.projectLink}`}>
+            <img className={projectsStyles.thumbnail}  src={TestImage}/>
+            <p  className={projectsStyles.projectTitle}>The Grove</p> 
+            </Link>
+          </div>
+          <div className={`${projectsStyles.projectWrapper}`}>
+            <Link to='/' className={`${projectsStyles.projectLink}`}>
+            <img className={projectsStyles.thumbnail}  src={TestImage}/>
+            <p  className={projectsStyles.projectTitle}>The Grove</p> 
+            </Link>
+          </div>
+          <div className={`${projectsStyles.projectWrapper}`}>
+            <Link to='/performance' className={`${projectsStyles.projectLink}`}>
+            <img className={projectsStyles.thumbnail}  src={TestImage}/>
+            <p  className={projectsStyles.projectTitle}>The Grove</p> 
+            </Link>
           </div>
       </div>
     </section>
