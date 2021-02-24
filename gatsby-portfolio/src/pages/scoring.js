488cb760d779f068403a5d4c1374dca25a7f53b5
Miles Grossenbacher <mrg498@nyu.edu>
2021-02-24 12:25:36 -0500

    ✨ formatted project thumbnail post

--- gatsby-portfolio/src/pages/scoring.js
+++ gatsby-portfolio/src/pages/scoring.js
@@ -3,12 +3,42 @@ import { Link } from "gatsby"
 
 import SEO from "../components/seo"
 
+import "../main.scss"
+import pageStyles from "./index.module.scss"
+import projectsStyles from "./projects.module.scss"
+
+import TestImage from "../images/thumbnails/scoring/000_scoring-th.png";
+
+
+const projects = [
+  {
+    thumbnail: ``,
+    image: ``,
+    title: `project1`,
+    credits: `Will Kahn, Alex Carr`,
+    projectDescription: ``,
+    personalDescription: ``
+  }
+]
+
+
 const Scoring = () => (
-  <>
+  <div className={projectsStyles.page} style={{backgroundColor: `rgb(172, 196, 196)`}}>
     <SEO title="Scoring" />
-    <h1>Scoring</h1>
-    <Link to="/">Go back to the homepage</Link>
-  </>
+    <section>
+      <h1 className={`${pageStyles.title} ${projectsStyles.pageTitle}`}>Scoring</h1>
+      <div className={`${projectsStyles.projectsContainer}`}>
+          <div className={`${projectsStyles.projectWrapper}`}>
+            <img className={projectsStyles.thumbnail}  src={TestImage}/>
+            <p className={projectsStyles.projectTitle}>The Grove</p> 
+          </div>
+          <div className={`${projectsStyles.projectWrapper}`}>
+            <img className={projectsStyles.thumbnail}  src={TestImage}/>
+            <p className={projectsStyles.projectTitle}>The Grove</p> 
+          </div>
+      </div>
+    </section>
+  </div>
 )
 
 export default Scoring
