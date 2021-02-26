7017ee9b4144a9ed8a8b4871495c4ee277bab258
Miles Grossenbacher <mrg498@nyu.edu>
2021-02-26 12:02:35 -0500

    🎨 divide posts into separate folders

--- gatsby-portfolio/content/posts/the-grove/index.md
+++
@@ -1,5 +0,0 @@
----
-title: The Grove
-description: "A film "
----
-This is a very spooky film with good audio.
--- gatsby-portfolio/gatsby-config.js
+++ gatsby-portfolio/gatsby-config.js
@@ -1,44 +1,59 @@
 module.exports = {
-  siteMetadata: {
-    title: `Alex Carr`,
-    description: `Alex Carr's professional and artistic portfolio`,
-    author: `@sandymilesg`,
-  },
-  plugins: [
-    `gatsby-plugin-smoothscroll`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/content`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-netlify-cms`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Alex Carr Portfolio`,
-        short_name: `Portfolio`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#000000`,
-        display: `minimal-ui`,
-        icon: `src/images/favicons/favicon-32x32.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+	siteMetadata: {
+		title: `Alex Carr`,
+		description: `Alex Carr's professional and artistic portfolio`,
+		author: `@sandymilesg`
+	},
+	plugins: [
+		`gatsby-plugin-smoothscroll`,
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`
+			}
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `content`,
+				path: `${__dirname}/content`
+			}
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				// CommonMark mode (default: true)
+				commonmark: true,
+				// Footnotes mode (default: true)
+				footnotes: true,
+				// Pedantic mode (default: true)
+				pedantic: true,
+				// GitHub Flavored Markdown mode (default: true)
+				gfm: true,
+				// Plugins configs
+				plugins: []
+			}
+		},
+		`gatsby-plugin-netlify-cms`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `Alex Carr Portfolio`,
+				short_name: `Portfolio`,
+				start_url: `/`,
+				background_color: `#ffffff`,
+				theme_color: `#000000`,
+				display: `minimal-ui`,
+				icon: `src/images/favicons/favicon-32x32.png` // This path is relative to the root of the site.
+			}
+		}
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	]
+};
