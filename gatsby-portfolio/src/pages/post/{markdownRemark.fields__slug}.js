9ad8ac66040eb1fd2cf053f63d4653a1ae6826ad
Miles Grossenbacher <mrg498@nyu.edu>
2021-02-27 08:11:26 -0500

    🎨 query post by slug variable

--- gatsby-portfolio/src/pages/post/{markdownRemark.fields__slug}.js
+++ gatsby-portfolio/src/pages/post/{markdownRemark.fields__slug}.js
@@ -2,17 +2,18 @@ import React from "react";
 import {graphql} from 'gatsby';
 
 export default function PostTemplate({ data }) {
+	const post = data.markdownRemark;
 	return (
 		<div>
-			<h1>Post Template</h1>
-            <p>{data.markdownRemark.internal.content}</p>
+			<h1>{post.frontmatter.title}</h1>
+            <p>{post.internal.content}</p>
 		</div>
 	);
 }
 
 export const query = graphql`
-	query($id: String!) {
-		markdownRemark(id: { eq: $id }) {
+	query($fields__slug: String!) {
+		markdownRemark(fields: {slug: {eq: $fields__slug}}) {
 			frontmatter {
 				title
 			}
